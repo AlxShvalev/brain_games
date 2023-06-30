684118c7ff39b7296361ae265059aed6e096a2df
Alex Shvalev <shvalevalx@gmail.com>
2023-06-30 12:07:09 +0500

    change models

--- app/core/db/models.py
+++ app/core/db/models.py
@@ -1,9 +1,11 @@
+import enum
 import uuid
 
-from sqlalchemy import DATE, TIMESTAMP, Column, Integer, String, func
+from sqlalchemy import DATE, TIMESTAMP, Column, Enum, String, func
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import declared_attr, relationship
+from sqlalchemy.schema import ForeignKey
 
 
 @as_declarative()
@@ -23,27 +25,36 @@ class Base:
 
 
 class User(Base):
-    """Модель полоьзователя."""
+    """Модель пользователя."""
 
-    username = Column(String(100), nullable=False)
+    class Role(str, enum.Enum):
+        ADMIN = "admin"
+        STUFF = "stuff"
+        USER = "user"
+
+    username = Column(String(100), nullable=False, unique=True)
     name = Column(String(100), nullable=False)
     surname = Column(String(100))
-    email = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
     hashed_password = Column(String(70), nullable=False)
+    role = Column(Enum(Role, name="user_role", values_callable=lambda obj: [e.value for e in obj]), nullable=False)
     date_of_birth = Column(DATE)
     last_login_at = Column(TIMESTAMP)
 
-    def __repr__(self) -> str:
-        return f"{self.name} {self.surname}"
+    def __repr__(self):
+        return self.username
 
 
 class Command(Base):
     """Модель для описания Команды."""
 
-    title = Column(String(500), nullable=False)
+    title = Column(String(500), nullable=False, unique=True)
     city = Column(String(100))
     players = relationship("Player", back_populates="commands")
     answers = relationship("Answer", back_populates="command")
+
+    def __repr__(self):
+        return self.title
 
 
 class Player(Base):
@@ -55,26 +66,32 @@ class Player(Base):
 class Game(Base):
     """Модель для описания Игры."""
 
-    title = Column(
-        String,
-    )
-    date_of = Column(TIMESTAMP)
+    title = Column(String, nullable=False, unique=True)
+    date_of = Column(DATE, nullable=False)
     questions = relationship("Question", back_populates="game")
     answers = relationship("Answer", back_populates="game")
+
+    def __repr__(self):
+        return self.title
 
 
 class Question(Base):
     """Модель для описания Вопроса."""
 
-    number = Column(Integer)
+    author_id = Column(UUID(as_uuid=True), ForeignKey(User.id, ondelete="CASCADE"), nullable=False)
+    author = relationship("User", back_populates="questions")
     text = Column(String(2048), nullable=False)
     answer = Column(String(500), nullable=False)
-    comment = Column(String(2048), nullable=True)
+    comment = Column(String(2048))
     game = relationship("Game", back_populates="questions")
+    answers = relationship("Answer", back_populates="question")
 
 
 class Answer(Base):
     """Модель для описания Ответа."""
 
-    text = Column(String)
+    text = Column(String(500))
+    command_id = Column(UUID(as_uuid=True), ForeignKey(Command.id, ondelete="CASCADE"), nullable=False)
     command = relationship("Command", back_populates="answers")
+    question_id = Column(UUID(as_uuid=True), ForeignKey(Question.id, ondelete="CASCADE"), nullable=False)
+    question = relationship("Question", back_populates="answers")
