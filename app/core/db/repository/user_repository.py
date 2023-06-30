263dce0eab3eaf1e395707ae7c0550344d1619e6
Alex Shvalev <shvalevalx@gmail.com>
2023-06-30 14:13:36 +0500

    added user repository

---
+++ app/core/db/repository/user_repository.py
@@ -0,0 +1,13 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db.db import get_async_session
+from app.core.db.models import User
+from app.core.db.repository.adbstract_repository import AbstractRepository
+
+
+class UserRepository(AbstractRepository):
+    """Репозиторий для работы с моделью User."""
+
+    def __init__(self, session: AsyncSession = Depends(get_async_session)) -> None:
+        super().__init__(session, User)
