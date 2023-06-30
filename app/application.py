4325ca820b257dbe4734ce4adf2539075c08e6c2
Alex Shvalev <shvalevalx@gmail.com>
2023-06-30 12:53:28 +0500

    Changed settings

--- app/application.py
+++ app/application.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
 
-from app.core.settings import settings
 from app.api.routers import command_router
+from app.core.settings import settings
 
 
 def create_app() -> FastAPI:
     """Create FastAPI application."""
-    app = FastAPI(
-        title=settings.title,
-        debug=settings.debug,
-        root_path=settings.root_path
-    )
+    app = FastAPI(title=settings.TITLE, debug=settings.DEBUG, root_path=settings.ROOT_PATH)
     app.include_router(command_router)
 
     return app
--- app/core/settings.py
+++ app/core/settings.py
@@ -3,40 +3,40 @@ from pathlib import Path
 
 from pydantic import BaseSettings
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 ENV_FILE = os.path.join(BASE_DIR / ".env")
 
 
 class Settings(BaseSettings):
-    """Application settings class."""
+    """Настройки приложения."""
+
+    DEBUG: bool = False
+
     # Hosting settings
-    host: str
-    ip_port: int
+    HOST: str
+    IP_PORT: int
 
     # Database settings
-    db_name: str
-    db_user: str
-    db_password: str
-    db_host: str
-    db_port: str
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
 
     # Application settings
-    title: str = "Brain Games"
-    debug: bool = False
-    reload: bool = False
-    root_path: str = ""
-    media_dir: str = "media"
+    TITLE: str = "Brain Games"
+    RELOAD: bool = False
+    ROOT_PATH: str = "/api/v1"
+    MEDIA_DIR: str = BASE_DIR / "media"
 
     @property
     def database_url(self) -> str:
         """Get db connection url."""
-        return "sqlite:///foo.db"
-        # return (
-        #     "postgresql+asyncpg://"
-        #     f"{self.db_user}:{self.db_password}"
-        #     f"@{self.db_host}:{self.db_port}/{self.db_name}"
-        # )
+        return (
+            "postgresql+asyncpg://"
+            f"{self.DB_USER}:{self.DB_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
 
     class Config:
         env_file = ENV_FILE
