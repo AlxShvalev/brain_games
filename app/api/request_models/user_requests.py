5441676bffee286cf64c3ed68f201a0edbca992a
Alex Shvalev <shvalevalx@gmail.com>
2023-06-30 16:18:14 +0500

    added fields to UserCreateRequest

--- app/api/request_models/user_requests.py
+++ app/api/request_models/user_requests.py
@@ -10,8 +10,10 @@ class UserCreateRequest(BaseModel):
         min_length=3, max_length=100, title="username", description="username, уникальный для каждого пользователя."
     )
     name: StrictStr = Field(min_length=1, max_length=100, title="Имя пользователя")
-    email: EmailStr = Field()
+    surname: StrictStr | None
+    email: EmailStr
     password: SecretStr
+    date_of_birth: date | None
 
 
 class UserUpdateRequest(BaseModel):
