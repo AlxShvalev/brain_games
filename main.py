579e0e7b6eac25ba2dd2fa0bc6a9d3d762e192d5
Alex Shvalev <shvalevalx@gmail.com>
2023-06-30 15:09:39 +0500

    refactored settings values

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app = create_app()
 
 if __name__ == "__main__":
     """Starting application."""
-    uvicorn.run(app, host=settings.host, port=settings.ip_port)
+    uvicorn.run(app, host=settings.HOST, port=settings.IP_PORT)
