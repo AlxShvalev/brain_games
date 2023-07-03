05963b4be1fe07da1b282923aabd24c5704f34d6
Alex Shvalev <shvalevalx@gmail.com>
2023-07-03 15:47:26 +0500

    Added authentication, pass change models

--- app/api/request_models/user_requests.py
+++ app/api/request_models/user_requests.py
@@ -6,12 +6,10 @@ from app.api.request_models.request_base import RequestBaseModel
 
 
 class UserCreateRequest(RequestBaseModel):
-    """Модель запроса для регистрации пользователя."""
+    """Тело запроса для регистрации пользователя."""
 
-    username: StrictStr = Field(
-        min_length=3, max_length=100, title="username", description="username, уникальный для каждого пользователя."
-    )
-    name: StrictStr = Field(min_length=1, max_length=100, title="Имя пользователя")
+    username: StrictStr = Field(min_length=3, max_length=100)
+    name: StrictStr = Field(min_length=1, max_length=100)
     surname: StrictStr | None
     email: EmailStr
     password: SecretStr
@@ -19,8 +17,22 @@ class UserCreateRequest(RequestBaseModel):
 
 
 class UserUpdateRequest(RequestBaseModel):
-    """Модель запроса для изменения пользователя."""
+    """Тело запроса для изменения пользователя."""
 
-    name: StrictStr
+    name: StrictStr | None
     surname: StrictStr | None
     date_of_birth: date | None
+
+
+class LoginRequest(RequestBaseModel):
+    """Тело запроса для аутентификации пользователя."""
+
+    username: StrictStr
+    password: SecretStr
+
+
+class ChangePasswordRequest(RequestBaseModel):
+    """Тело запроса для смены пароля."""
+
+    current_password: SecretStr
+    new_password: SecretStr
