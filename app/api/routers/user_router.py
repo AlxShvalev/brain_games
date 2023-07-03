1ff2cfc75316abf6f0229c7240ee40dbccaaf479
Alex Shvalev <shvalevalx@gmail.com>
2023-07-03 09:52:48 +0500

    create and get functions for user

--- app/api/routers/user_router.py
+++ app/api/routers/user_router.py
@@ -1,11 +1,42 @@
-from fastapi import APIRouter
+from http import HTTPStatus
+from uuid import UUID
+
+from fastapi import APIRouter, Depends
+from fastapi_restful.cbv import cbv
 
 from app.api.request_models.user_requests import UserCreateRequest
+from app.api.response_models.user_response import UserResponse
+from app.core.services.user_service import UserService
 
 router = APIRouter(prefix="/users", tags=["Пользователи"])
 
 
-@router.post("/")
-async def create_user(user_schema: UserCreateRequest) -> UserCreateRequest:
-    """Создать пользователя."""
-    return user_schema
+@cbv(router)
+class UserCBV:
+    """Базовый класс для отображения пользователей."""
+
+    user_service: UserService = Depends()
+
+    @router.post(
+        "/",
+        response_model=UserResponse,
+        response_model_exclude_none=True,
+        status_code=HTTPStatus.CREATED,
+        summary="Регистрация нового пользователя.",
+        response_description="Регистрация нового пользователя.",
+    )
+    async def create_user(self, schema: UserCreateRequest) -> UserResponse:
+        """Регистрация нового пользователя."""
+        return await self.user_service.register_new_user(schema)
+
+    @router.get(
+        "/{user_id}",
+        response_model=UserResponse,
+        response_model_exclude_none=True,
+        status_code=HTTPStatus.OK,
+        summary="Получить пользователя",
+        response_description="Получить пользователя по его id.",
+    )
+    async def get_user(self, user_id: UUID) -> UserResponse:
+        """Получить пользователя по его id."""
+        return await self.user_service.get_user_by_id(user_id)
--- app/core/services/user_service.py
+++ app/core/services/user_service.py
@@ -1,23 +1,39 @@
 from uuid import UUID
 
 from fastapi import Depends
+from passlib.context import CryptContext
 
 from app.api.request_models.user_requests import UserCreateRequest, UserUpdateRequest
 from app.api.response_models.user_response import UserResponse
 from app.core.db.models import User
 from app.core.db.repository.user_repository import UserRepository
 
+PASSWORD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 class UserService:
     def __init__(self, user_repository: UserRepository = Depends()):
         self.__user_repository = user_repository
 
-    async def register_new_user(self, user_schema: UserCreateRequest) -> UserResponse:
+    def _get_hashed_password(self, password: str) -> str:
+        """Получить хеш пароля."""
+        return PASSWORD_CONTEXT.hash(password)
+
+    def _verify_hashed_password(self, password: str, hashed_password: str) -> bool:
+        return PASSWORD_CONTEXT.verify(password, hashed_password)
+
+    async def register_new_user(self, schema: UserCreateRequest) -> UserResponse:
         """Регистрация пользователя."""
-        user = User(**user_schema.dict())
-        user.role = User.Role.USER
-        user = await self.__user_repository.create(user)
-        return user
+        user = User(
+            username=schema.username,
+            email=schema.email,
+            name=schema.name,
+            surname=schema.surname,
+            role=User.Role.USER,
+            hashed_password=self._get_hashed_password(schema.password.get_secret_value()),
+            date_of_birth=schema.date_of_birth,
+        )
+        return await self.__user_repository.create(user)
 
     async def get_user_by_id(self, id: UUID) -> UserResponse:
         """Получить пользователя."""
