c18c74a331752b67d5d06958ffbc7a3a1ddf2113
Alex Shvalev <shvalevalx@gmail.com>
2023-07-03 09:55:04 +0500

    fix typo in abstract_repositore.py

--- app/core/db/repository/user_repository.py
+++ app/core/db/repository/user_repository.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db.db import get_async_session
 from app.core.db.models import User
-from app.core.db.repository.adbstract_repository import AbstractRepository
+from app.core.db.repository.abstract_repository import AbstractRepository
 
 
 class UserRepository(AbstractRepository):
