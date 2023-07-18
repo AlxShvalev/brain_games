99ac5cfbffaeee6c48f0cab418496678d5c27883
Alex Shvalev <shvalevalx@gmail.com>
2023-07-18 09:38:08 +0500

    update docstrings in pydantic models

--- app/api/request_models/user_requests.py
+++ app/api/request_models/user_requests.py
@@ -6,7 +6,16 @@ from app.api.request_models.request_base import RequestBaseModel
 
 
 class UserCreateRequest(RequestBaseModel):
-    """Тело запроса для регистрации пользователя."""
+    """
+    Тело запроса для регистрации пользователя.
+
+    - **username**: уникальный юзернейм пользователя;
+    - **name**: имя пользователя;
+    - **surname**: фамилия пользователя _(не обязательное поле)_;
+    - **email**: email пользователя;
+    - **password**: пароль пользователя;
+    - **date_of_birth**: дата рождения в формате ISO 8601 _(не обязательно поле)_.
+    """
 
     username: StrictStr = Field(min_length=3, max_length=100)
     name: StrictStr = Field(min_length=1, max_length=100)
@@ -17,7 +26,14 @@ class UserCreateRequest(RequestBaseModel):
 
 
 class UserUpdateRequest(RequestBaseModel):
-    """Тело запроса для изменения пользователя."""
+    """
+    Тело запроса для изменения пользователя.
+
+    Доступные для изменения поля:
+    - **name**: имя пользователя _(не обязательное поле)_;
+    - **surname**: фамилия пользователя _(не обязательное поле)_;
+    - **date_of_birth**: дата рождения в формате ISO 8601 _(не обязательно поле)_.
+    """
 
     name: StrictStr | None
     surname: StrictStr | None
@@ -25,14 +41,24 @@ class UserUpdateRequest(RequestBaseModel):
 
 
 class LoginRequest(RequestBaseModel):
-    """Тело запроса для аутентификации пользователя."""
+    """
+    Тело запроса для аутентификации пользователя.
+
+    - **username**: юзернейм пользователя;
+    - **password**: пароль пользователя.
+    """
 
     username: StrictStr
     password: SecretStr
 
 
 class ChangePasswordRequest(RequestBaseModel):
-    """Тело запроса для смены пароля."""
+    """
+    Тело запроса для смены пароля.
+
+    - **current_password**: текущий пароль пользователя;
+    - **new_password**: новый пароль пользователя.
+    """
 
     current_password: SecretStr
     new_password: SecretStr
--- app/api/response_models/user_response.py
+++ app/api/response_models/user_response.py
@@ -5,7 +5,16 @@ from pydantic import BaseModel
 
 
 class UserResponse(BaseModel):
-    """Тело ответа для модели User."""
+    """
+    Тело ответа для модели User.
+
+    - **id**: уникальный id пользователя в формате UUID;
+    - **username**: уникальный юзернейм пользователя;
+    - **name**: имя пользователя;
+    - **surname**: фамилия пользователя _(необязательное поле)_;
+    - **email**: email пользователя;
+    - **last_login_at**: дата последнего входа в формате ISO 8601 _(необязательное поле)_.
+    """
 
     id: UUID
     username: str
@@ -19,6 +28,12 @@ class UserResponse(BaseModel):
 
 
 class UserLoginResponse(BaseModel):
-    """Тело ответа при аутентификации."""
+    """
+    Тело ответа при аутентификации.
+
+    - **access_token**: токен доступа;
+    - **refresh_token** токен для генерации нового токена доступа.
+    """
+
     access_token: str
     refresh_token: str
