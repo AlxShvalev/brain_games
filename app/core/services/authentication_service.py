c2520da5f6fa8ba11a1d317286a13771e86322b2
Alex Shvalev <shvalevalx@gmail.com>
2023-11-03 12:53:32 +0500

    New user authentication methods

--- app/core/services/authentication_service.py
+++ app/core/services/authentication_service.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 
 from app.api.request_models.user_requests import LoginRequest
@@ -52,11 +52,38 @@ class AuthenticationService:
         to_encode = {"username": username, "exp": expire}
         return jwt.encode(to_encode, settings.SECRET_KEY, ALGORITHM)
 
+    @staticmethod
+    def _get_username_from_token(token: str) -> str:
+        """Возвращает username из токена."""
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
+        except JWTError:
+            raise exceptions.UnauthorizedError
+        username = payload.get("username")
+        if not username:
+            raise exceptions.UnauthorizedError
+        return username
+
     async def login(self, auth_data: LoginRequest) -> UserLoginResponse:
         """Получить access- и refresh- токены."""
         user = await self.__authenticate_user(auth_data)
         user.last_login_at = dt.datetime.now()
         await self.__user_repository.update(user.id, user)
+        return UserLoginResponse(
+            access_token=self.__create_jwt_token(user.username, settings.ACCESS_TOKEN_EXPIRES_MINUTES),
+            refresh_token=self.__create_jwt_token(user.username, settings.REFRESH_TOKEN_EXPIRES_MINUTES),
+        )
+
+    async def get_current_user(self, token: str) -> User:
+        """Получить текущего активного пользователя по токену."""
+        username = self._get_username_from_token(token)
+        return await self.__user_repository.get_by_username(username)
+
+    async def refresh(self, refresh_token: str | None) -> UserLoginResponse:
+        """Получить новую пару refresh- и access- токенов."""
+        if not refresh_token:
+            raise exceptions.UnauthorizedError
+        user = await self.get_current_user(refresh_token)
         return UserLoginResponse(
             access_token=self.__create_jwt_token(user.username, settings.ACCESS_TOKEN_EXPIRES_MINUTES),
             refresh_token=self.__create_jwt_token(user.username, settings.REFRESH_TOKEN_EXPIRES_MINUTES),
