b5ee845ca686f426c088111db23729102cfaad19
Alex Shvalev <shvalevalx@gmail.com>
2023-11-15 15:52:29 +0500

    added custom exception handlers

--- app/application.py
+++ app/application.py
@@ -1,3 +1,10 @@
+from http import HTTPStatus
+
+from core import exceptions
+from core.exceptions_handlers import (
+    application_error_handler,
+    internal_exception_handler,
+)
 from fastapi import FastAPI
 
 from app.api.routers import command_router, user_router
@@ -9,5 +16,8 @@ def create_app() -> FastAPI:
     app = FastAPI(title=settings.TITLE, debug=settings.DEBUG, root_path=settings.ROOT_PATH)
     app.include_router(command_router)
     app.include_router(user_router)
+
+    app.add_exception_handler(HTTPStatus.INTERNAL_SERVER_ERROR, internal_exception_handler)
+    app.add_exception_handler(exceptions.ApplicationError, application_error_handler)
 
     return app
--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -20,6 +20,11 @@ class NotFoundError(ApplicationError):
     status_code: HTTPStatus = HTTPStatus.NOT_FOUND
 
 
+class UnauthorizedError(ApplicationError):
+    status_code: HTTPStatus = HTTPStatus.UNAUTHORIZED
+    detail = "У Вас нет прав для просмотра запрошенной страницы."
+
+
 class ObjectAlreadyExistsError(BadRequestError):
     def __init__(self, model: DatabaseModel):
         self.detail = f"Объект '{model.__repr__()}' уже существует."
