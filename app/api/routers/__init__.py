478609db805103cfd6e253f75192efa1af83285d
Alex Shvalev <shvalevalx@gmail.com>
2023-11-16 14:46:56 +0500

    Rename Command to Team

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -1,2 +1,2 @@
-from .command_routers import router as command_router  # noqa F401
+from .team_routers import router as team_router  # noqa F401
 from .user_router import router as user_router  # noqa F401
--- app/application.py
+++ app/application.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from app.api.routers import command_router, user_router
+from app.api.routers import team_router, user_router
 from app.core import exceptions
 from app.core.exceptions_handlers import (
     application_error_handler,
@@ -14,7 +14,7 @@ from app.core.settings import settings
 def create_app() -> FastAPI:
     """Create FastAPI application."""
     app = FastAPI(title=settings.TITLE, debug=settings.DEBUG, root_path=settings.ROOT_PATH)
-    app.include_router(command_router)
+    app.include_router(team_router)
     app.include_router(user_router)
 
     app.add_exception_handler(HTTPStatus.INTERNAL_SERVER_ERROR, internal_exception_handler)
